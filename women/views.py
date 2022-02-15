e81acedd642303356cb1e48bcecea1dcda45baf2
CheMan <cherry_diman@mail.ru>
2022-02-15 18:13:01 +0200

    Initial

---
+++ women/views.py
@@ -0,0 +1,66 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404
+
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'},
+        ]
+
+
+def index(request):
+
+    context = {
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+
+    return render(request, 'women/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена. Бывает :)</h1>")
+
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'women/post.html', context=context)
+
+
+def show_category(request, cat_slug):
+    cats = get_object_or_404(Category, slug=cat_slug)
+
+    context = {
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cats.pk,
+    }
+
+    return render(request, 'women/index.html', context=context)
