b03fb3e57b703c8ed4eae10ba94b1309cc433b0f
CheMan <cherry_diman@mail.ru>
2022-02-24 17:05:21 +0200

    Added authentication users and logout

--- women/views.py
+++ women/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -39,10 +41,6 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 
 def contact(request):
     return HttpResponse("Обратная связь")
-
-
-def login(request):
-    return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
@@ -86,3 +84,26 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Регистрация")
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('main')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('main')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
